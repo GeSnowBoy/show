80c10179b46dfa2c29e2e219be9fa1895af098b0
yuanSnowBoy.@162.com <yuanSonwBoy.@163.com>
2016-06-11 12:41:49 +0800

    个人网站

---
+++ js/index2.js
@@ -0,0 +1,33 @@
+
+/*无缝滚动*/
+
+window.onload=function(){
+	var speed=30;
+	var box=document.getElementById("box1");
+	var left=document.getElementById("left");
+	var right=document.getElementById("right");
+
+	right.innerHTML=left.innerHTML;
+
+	var time=setInterval(fun,speed);
+	function fun()
+	{
+		if(left.offsetWidth-box.scrollLeft<=0)
+		{
+			box.scrollLeft=0;
+		}
+		else
+		{
+			box.scrollLeft++;
+		}
+	}
+
+	box.mouseover(function(event) {
+		clearInterval(time);
+
+	},function(){
+		
+		time=setInterval(fun, speed);
+	});
+}
+
